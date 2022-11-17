b700f62c30816d574957a3961bd4e2b07fd88816
Daniel <danielbougleux@id.uff.br>
2022-11-16 22:03:07 -0300

    versão inicial da função que separa os operandos

--- createTree.hs
+++ createTree.hs
@@ -2,4 +2,14 @@ data Tree a = Node a Bool Int (Tree a) (Tree a) | Empty deriving (Show)
 
 makeTree :: String -> Tree Char
 makeTree [] = Empty
-makeTree (a : t) = Node a True (makeTree t) (makeTree t)
+makeTree (a : t) = Node a True 2 (makeTree t) (makeTree t)
+
+readOperator :: String -> String -> Int -> (String, String, String)
+readOperator (c : t) accstr accpar
+    | (c == '>' || c == '^' || c == 'v' || c == '~') && (accpar == 1) = (accstr, [c], t)
+    | (c == '(') && (accpar == 0) = readOperator (init t) accstr (accpar+1) 
+    | (c == '(') && (accpar > 0) = readOperator t (accstr ++ [c]) (accpar+1)
+    | (c == ')') = readOperator t (accstr ++ [c]) (accpar-1)
+    | c == ' ' = readOperator t accstr accpar
+    | otherwise = readOperator t (accstr ++ [c]) accpar
+    
