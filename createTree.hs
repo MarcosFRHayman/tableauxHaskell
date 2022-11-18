5febb2ac9a8a47e5a6602e28e3de4d8029465c9d
Daniel <danielbougleux@id.uff.br>
2022-11-17 21:48:03 -0300

    adicionada a função que cria a árvore de refutação

--- createTree.hs
+++ createTree.hs
@@ -1,15 +1,38 @@
-data Tree a = Node a Bool Int (Tree a) (Tree a) | Empty deriving (Show)
+data Tree a = Node a Bool (Tree a) (Tree a) | Empty deriving (Show)
 
-makeTree :: String -> Tree Char
-makeTree [] = Empty
-makeTree (a : t) = Node a True 2 (makeTree t) (makeTree t)
+createTree :: [(String, Bool)] -> Tree String
+createTree [] = Empty
+createTree ((formula, valorVerdade) : resto)
+    | (esq == " ") && (dir == " ") = Node formula valorVerdade (createTree resto) Empty
+    | (op == '>') && valorVerdade = Node formula valorVerdade (createTree (resto++[(esq, False)])) (createTree (resto++[(dir, True)]))
+    | (op == '>') && (not valorVerdade) = Node formula valorVerdade (createTree (resto++[(esq, True),(dir, False)])) Empty
+    | (op == '^') && valorVerdade = Node formula valorVerdade (createTree (resto++[(esq, True),(dir, True)])) Empty
+    | (op == '^') && (not valorVerdade) = Node formula valorVerdade (createTree (resto++[(esq, False)])) (createTree (resto++[(dir, False)]))
+    | (op == 'v') && valorVerdade = Node formula valorVerdade (createTree (resto++[(esq, True)])) (createTree (resto++[(dir, True)]))
+    | (op == 'v') && (not valorVerdade) = Node formula valorVerdade (createTree (resto++[(esq, False),(dir, False)])) Empty
+    | (op == '~') && valorVerdade = Node formula valorVerdade (createTree (resto++[(dir, False)])) Empty
+    | (op == '~') && (not valorVerdade) = Node formula valorVerdade (createTree (resto++[(dir, True)])) Empty
+    where
+        resp = readOperator formula "" 0
+        (esq, op, dir) = resp
 
-readOperator :: String -> String -> Int -> (String, String, String)
+readOperator :: String -> String -> Int -> (String, Char, String)
+readOperator [] _ _ = (" ", 'E', " ")
 readOperator (c : t) accstr accpar
-    | (c == '>' || c == '^' || c == 'v' || c == '~') && (accpar == 1) = (accstr, [c], t)
+    | (c == '>' || c == '^' || c == 'v' || c == '~') && (accpar == 1) = (accstr, c, t)
     | (c == '(') && (accpar == 0) = readOperator (init t) accstr (accpar+1) 
     | (c == '(') && (accpar > 0) = readOperator t (accstr ++ [c]) (accpar+1)
     | (c == ')') = readOperator t (accstr ++ [c]) (accpar-1)
     | c == ' ' = readOperator t accstr accpar
     | otherwise = readOperator t (accstr ++ [c]) accpar
-    
+
+indent :: [String] -> [String]
+indent x = map ("     "++) x
+
+layoutTree :: Show a => Tree a -> [String]
+layoutTree Empty = []
+layoutTree (Node here truth left right)
+            = indent (layoutTree right) ++ [show truth] ++[show here] ++ indent (layoutTree left)
+
+prettyTree :: Show a => Tree a -> String
+prettyTree = unlines.layoutTree
